723dea224d3128bf2c3f8f08e15b7ffc78de1767
SURYA <Suryachiru91@gmail.com>
2022-05-09 09:43:08 +0530

    countryList

---
+++ countryList.js
@@ -0,0 +1,57 @@
+// Using the countries array create the following array of arrays. The country name, the first three letters of the country name and the length of the country name.
+
+const countryList = [
+    'ALBANIA',
+  'BOLIVIA',
+  'CANADA',
+  'DENMARK',
+  'ETHIOPIA',
+  'FINLAND',
+  'GERMANY',
+  'HUNGARY',
+  'IRELAND',
+  'JAPAN',
+  'KENYA'
+];
+
+const countryList3 = {
+  "ALB": "Albania",
+  "BOL": "Bolivia",
+  "CAN": "Canada",
+  "DEN": "Denmark",
+  "ETH": "Ethiopia",
+  "FIN": "Finland",
+  "GER": "Germany",
+  "HUN": "Hungary",
+  "IRE": "IRELAND",
+  "JAP": "Japan",
+  "KEN": "Kenya",
+};
+
+const countryListLength ={
+  "7": "Albania",
+  "7": "Bolivia",
+  "6": "Canada",
+  "7": "Denmark",
+  "8": "Ethiopia",
+  "7": "Finland",
+  "7": "Germany",
+  "7": "Hungary",
+  "7": "Ireland",
+  "5": "Japan",
+  "5": "Kenya",
+};
+
+const countryListAllIsoData = [
+  {"code3":  "ALB", "name": "Albania", "length": "7"},
+  {"code3": "BOL","name": "Bolivia", "length": "7"},
+  {"code3": "CAN", "name": "Canada", "length": "6"},
+  {"code3": "DEN", "name": "Denmark", "length": "7"},
+  {"code3": "ETH", "name": "Ethiopia", "length": "8"},
+  {"code3": "FIN", "name": "Finland", "length": "7"},
+  {"code3": "GER", "name": "Germany", "length": "7"},
+  {"code3": "HUN", "name": "Hungary", "length": "7"},
+  {"code3": "IRE", "name": "IRE", "length": "7"},
+  {"code3": "JAN", "name": "Japan", "length": "5"},
+  {"code3": "KEN", "name": "Kenya", "length": "5"},
+];
